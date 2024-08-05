23517653c3ba6b1beb85392a96b6720665c53125
yanghyeyeon <yhy4058@gmail.com>
2024-08-05 19:13:00 +0900

    [FEAT] main page

--- src/main/java/com/jjikmukpa/project/main/controller/MainController.java
+++ src/main/java/com/jjikmukpa/project/main/controller/MainController.java
@@ -2,7 +2,6 @@ package com.jjikmukpa.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
